0ac104faf8a4147f28276ec900f164ba45695846
Reinaldo <landaetareinaldo143@gmail.com>
2024-06-21 01:06:29 -0400

    two commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-? 'placeholder'
+? 'https://prevencrime-prod.onrender.com'
 : 'http://localhost:3005'
 
 const MONGO_URI = process.env.NODE_ENV === 'production'
