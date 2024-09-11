ea7461cfc54cce6af5016c16a4d2ca7b033e97b2
Reinaldo Landaeta <144262402+REyyyy43@users.noreply.github.com>
2024-09-11 11:49:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const app = require('./app');
 const http = require('http');
 
+const PORT = process.env.PORT || 3005; // Usa el puerto asignado por Render o 3005 como fallback
 
 const server = http.createServer(app);
 
-
-server.listen(3005, () => {
-
-    console.log('El servidor esta corriendo');
-    
+server.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}`);
 });
